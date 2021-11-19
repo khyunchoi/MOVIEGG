6b9da768cfc86aa52b6cc04ba1dab807bb1627b3
beom <habem3618@naver.com>
2021-11-19 09:19:37 +0900

    Add[1118 models.py]

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    profile_image = models.ImageField(upload_to='profile_image/', null=True, blank=True)
+    nickname = models.CharField(max_length=20)
+    exp = models.IntegerField()
